6a030fee0a5b504c6194e97ed218c73cf6d5846f
tanvirkamble <tanukamble22@gmail.com>
2023-08-12 14:54:43 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> favourite food</h1>
+    <ul>
+      <li> one </li>
+      <li> one </li>
+      <li> one </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
